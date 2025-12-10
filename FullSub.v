f95e479be314cbedf9c89dce2bcca057d4d64f6d
vkarthi-20 <vkarthi20vks@gmail.com>
2025-12-10 14:48:38 +0530

    Add files via upload

---
+++ FullSub.v
@@ -0,0 +1,10 @@
+module FullSub (
+    input  wire a, b, bin,       // Inputs
+    output wire diff, borrow     // Outputs
+);
+
+    // Logic equations
+    assign diff   = a ^ b ^ bin;                  // Difference
+    assign borrow = (~a & b) | (~(a ^ b) & bin);  // Borrow logic
+
+endmodule
